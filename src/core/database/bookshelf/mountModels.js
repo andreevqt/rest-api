97382534f77c40e585156f24da45beab618ccde1
qprquo <qprquo@gmail.com>
2021-04-22 16:14:25 +0300

    Updates

--- src/core/database/bookshelf/mountModels.js
+++ src/core/database/bookshelf/mountModels.js
@@ -10,59 +10,6 @@ const modelsUtils = require(`../../utils/models`);
 const {runMigrations} = require(`./schemaBuilder`);
 
 
-
-/* const mountModels = async () => {
-  const {modelsPath} = app;
-  const {orm, connector: {schemaBuilder}} = app.db;
-
-  const mountModel = async (definition) => {
-    await schemaBuilder.runMigrations({definition, orm});
-
-    const loadedModel = _.assign({
-      requireFetch: false,
-      tableName: definition.settings.tableName,
-      associations: [],
-    }, definition.options);
-
-    return orm.Model.extend(loadedModel);
-  };
-
-  const files = await fs.readdir(modelsPath);
-
-  const coreStore = await mountModel(coreStoreModel);
-  app.db.models = {'core_store': coreStore};
-
-  const models = await files.reduce(async (res, file) => {
-    const name = path.parse(file).name;
-    const plural = pluralize(name);
-
-    const definition = require(`${modelsPath}/${file}`);
-    definition.uid = definition.uid || `model::${plural}`;
-    definition.settings.tableName = definition.settings.tableName || plural;
-    definition.client = getClient();
-    definition.associations = [];
-    definition.options = definition.options || {};
-
-    
-    Object.keys(definition.attributes).forEach((name) => {
-      const details = definition.attributes[name];
-      if (details.type !== undefined) {
-        return;
-      }
-
-      modelsUtils.defineAssociations(definition, details. name);
-    })
-    modelsUtils.defineAssociations(definition);
-
-    const model = await mountModel(definition);
-    await storeDefinition(definition, orm);
-
-    return Promise.resolve({...res, [name]: model});
-  }, Promise.resolve({}));
-
-  app.db.models = {...app.db.models, ...models};
-} */
-
 const mountModels = async ({models, target}, {orm}) => {
   const getClient = () => {
     return process.env.NODE_ENV === `test` ? `sqlite3` : app.config.db.client;
